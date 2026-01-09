beeed8d9f343fe2bbb6d6bbbbf8cf8be3af8e3e7
Aurelia <aurelia-bot@aurelia.com>
2026-01-09 17:02:13 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -84,7 +84,7 @@ function runSetupCommands() {
     "bun add @dnd-kit/utilities",
     "bun add @dnd-kit/core @dnd-kit/utilities",
     "bun add @radix-ui/react-tooltip",
-    "bun add -D electron-builder",
+    "bun add -D concurrently wait-on electron electron-builder",
     "bun add react-hotkeys-hook",
     "bun add electron-builder --dev",
     "bun add date-fns"
